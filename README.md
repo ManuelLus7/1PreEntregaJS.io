e962e0f0d60fb5a03770ccd94cf23d198c849d0a
ManuelLus7 <ManuelLus7@users.noreply.github.com>
2023-04-09 18:42:50 -0300

    Version 1.0 Subiendo el Primer Codigo

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# 1PreEntregaJS
-Comisión: 39510
-Alumno: Manuel Lus
-1º Pre-Entrega Curso JavaScript - 
-Simulador de Cotización Seguros - Rama Cauciones
+<h1># 1PreEntregaJS</h1>
+<p>Comisión: 39510</p>
+<br>
+<p>Alumno: Manuel Lus</p>
+<p>1º Pre-Entrega Curso JavaScript</p> 
+<p>Simulador de Cotización Seguros - Rama Cauciones</p>
+<br>
+<p>Versión: 1.0</p>
+
+<h2>Definición: SEGURO DE CAUCION</h2>
+<p>Un seguro de caución es un contrato de seguro que garantiza el cumplimiento de una obligación o contrato por parte del tomador del seguro, ante un tercero beneficiario.</p>
+<p>Si el tomador incumple, la aseguradora indemniza al beneficiario según los límites establecidos.</p>
+<p>Este seguro se usa como garantía en operaciones como contrataciones de la Obras Públicas</p>
+
 
---
+++ cotizador.js
@@ -0,0 +1,69 @@
+const tasaAnual= 2.20;
+  const trimestral= (tasaAnual/4);
+  const mantenimientoOferta = "Mantenimiento de Oferta";
+  const ejecucionContrato = "Ejecución de Contrato";
+  const anticipoFinanciero = "Anticipo Financiero";
+  const fondoReparo = "Fondo de Reparo";
+  const bienvenido = "Bienvenido!! "
+  const menu = " ¿Qué Seguro de Caución desea Cotizar? \n 1-Mantenimiento de Oferta\n 2-Ejecución de Contrato\n 3-Anticipo Financiero\n 4-Fondo de Reparo\n 5-Salir"
+  const siguienteCotizacion = "Desea Cotizar algún Otro Seguro "
+  const valorOriginal = 0.00
+  const valorFormateado = new Intl.NumberFormat("es-ES", {
+    maximumFractionDigits: 2,
+  }).format(valorOriginal)
+
+  let usuario = prompt("Ingrese su Nombre por Favor...");
+  let opcion = parseInt(prompt(bienvenido+usuario+ " a Cwin Broker de Seguros:\n Su Tasa Anual actualmente es "+tasaAnual+" %\n\n"+menu));
+  let nombreObra = prompt("Ingrese el Nombre de la Obra");
+  let sumaAsegurada = prompt("Ingrese el Importe de la Suma Asegurada que necesita");
+  let cotizacion = trimestral;
+  let total = valorOriginal; 
+  
+
+  if (sumaAsegurada <= 333333333) {
+    mantenimiento = 2000+ "\n  Este Importe es el Mínimo Vigente para el Cobro de su PRIMA";
+    ejecución = 2000+ "\n  Este Importe es el Mínimo Vigente para el Cobro de su PRIMA";
+    anticipo = 2000+ "\n  Este Importe es el Mínimo Vigente para el Cobro de su PRIMA";
+    fondo = 2000+ "\n  Este Importe es el Mínimo Vigente para el Cobro de su PRIMA";
+  } else if (sumaAsegurada > 333333333.01) {
+    mantenimiento = Math.round(((sumaAsegurada*1/100)*(cotizacion))/100);
+    ejecución = Math.round(((sumaAsegurada*5/100)*(cotizacion))/100);
+    anticipo = Math.round(((sumaAsegurada*30/100)*(cotizacion))/100);
+    fondo = Math.round(((sumaAsegurada*5/100)*(cotizacion))/100);
+  }
+ 
+
+while (opcion !=5) {
+    switch (opcion) {
+        case 1:
+            total = ` es de $: ${mantenimiento}`;
+            alert(`Su Tasa Anual Actual es de: ${tasaAnual}%\n Detalle de Su Cotización:\n- Tipo de Póliza: ${mantenimientoOferta}\n- Nombre de la Obra: ${nombreObra}\n- Suma Asegurada: $ ${sumaAsegurada}\n- Su PRIMA de Seguro: ${total}`);
+              break;
+           
+        case 2:
+            total = ` es de $: ${ejecución}`;
+                alert(`Su Tasa Anual Actual es de: ${tasaAnual}%\n Detalle de Su Cotización:\n- Tipo de Póliza: ${ejecucionContrato}\n- Nombre de la Obra: ${nombreObra}\n- Suma Asegurada: $ ${sumaAsegurada}\n- Su PRIMA de Seguro: ${total}`);
+              break;
+            
+            case 3:
+                total = ` es de $: ${anticipo}`;
+                alert(`Su Tasa Anual Actual es de: ${tasaAnual}%\n Detalle de Su Cotización:\n- Tipo de Póliza: ${anticipoFinanciero}\n- Nombre de la Obra: ${nombreObra}\n- Suma Asegurada: $ ${sumaAsegurada}\n- Su PRIMA de Seguro: ${total}`);
+
+              break;
+                
+            case 4:
+                total = ` es de $: ${fondo}`;
+              alert(`Su Tasa Anual Actual es de: ${tasaAnual}%\n Detalle de Su Cotización:\n- Tipo de Póliza: ${fondoReparo}\n- Nombre de la Obra: ${nombreObra}\n- Suma Asegurada: $ ${sumaAsegurada}\n- Su PRIMA de Seguro: ${total}`);
+              break;
+            default:
+                break;
+                
+        }
+        
+        opcion = parseInt(prompt(siguienteCotizacion+usuario+menu));
+
+        if (opcion===5) {
+          console.log("Gracias por Utilizar Nuestro Simulador de Seguros de Caución \n Curso: JavaScript \n Comisión: 39510 \n Alumno: Manuel Lus");
+          alert("Gracias por Utilizar Nuestro Simulador de Seguros de Caución \n Curso: JavaScript \n Comisión: 39510 \n Alumno: Manuel Lus");
+      }
+}
